dea68c630a7e102c1be35b348a936d38458293d9
Your Github User Name <melquanmgreen95@gmail.com>
2021-11-04 16:27:12 -0400

    save before revert

--- scripts/weather/WeatherCard.js
+++ scripts/weather/WeatherCard.js
@@ -5,7 +5,6 @@ export const Weather = (weather) => {
       eachWeatherHTML += `
 
     <section id="weather_card">
-<<<<<<< HEAD
         <ul class="weather_type">
             <li> Temperature: ${weather[i].main.temp} F </li>
             <li> Feels like: ${weather[i].main.feels_like} F</li>
@@ -13,9 +12,7 @@ export const Weather = (weather) => {
             <li> High Temperature: ${weather[i].main.temp_max} F </li>
             <li> Humidity: ${weather[i].main.humidity}</li>
         </ul>
-=======
-    <div class="weather_type">${weather.data.}</div>
->>>>>>> parent of 39f2213 (Merge pull request #13 from NewForce-Cohort-5/bdHolidayRoad)
+    <div class="weather_type">${null}}</div>
     </section>
     `
 
@@ -23,3 +20,4 @@ export const Weather = (weather) => {
 console.log(eachWeatherHTML)
 return eachWeatherHTML
 }
+63f70ebcb068c5caa095bfcbc9bbab30d840d15c
