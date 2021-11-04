77123eacff3c83922f4a68c47180fef5ba599fc0
Your Github User Name <melquanmgreen95@gmail.com>
2021-11-04 16:27:22 -0400

    Revert "new4"
    This reverts commit 63f70ebcb068c5caa095bfcbc9bbab30d840d15c.
    
    :q!
    ]

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,5 @@
 export const settings = {
 	graphhopperKey: "",
-	npsKey: "IDFYIA8FpIW8iKzxdTjnknA2yIoxxYeJUQTrt8dG",
-	weatherKey: "73597cff891c8f2b18f8c91a537a3fd3"
+	npsKey: "",
+	weatherKey: ""
 };
