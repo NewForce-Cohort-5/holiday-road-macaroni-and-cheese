b7d7233e6cb862719e7d9881f3a6383404d53949
Your Github User Name <melquanmgreen95@gmail.com>
2021-11-05 13:39:54 -0400

    mvp and css done

--- scripts/weather/WeatherCard.js
+++ scripts/weather/WeatherCard.js
@@ -6,6 +6,7 @@ export const Weather = (weather) => {
 
     <section id="weather_card">
         <ul class="weather_type">
+            <li> Date: ${weather[i].dt_txt} </li>
             <li> Temperature: ${weather[i].main.temp} <span>&#8457;</span> </li>
             <li> Feels like: ${weather[i].main.feels_like} <span>&#8457;</span></li>
             <li> Low Temperature: ${weather[i].main.temp_min} <span>&#8457;</span> </li>
